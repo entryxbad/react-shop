6016b57d4db0b1777d62c83541fd29403e656371
exb <radmir_96@mail.ru>
2023-07-04 17:40:00 +0500

    gh-pages added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,12 @@ const Footer = () => {
             <div className='footer-copyright'>
                 <div className='container'>
                     © {new Date().getFullYear()} Copyright Text
-                    <a className='grey-text text-lighten-4 right' href='#!'>
+                    <a
+                        className='grey-text text-lighten-4 right'
+                        href='https://github.com/entryxbad/react-shop'
+                        rel='noreferrer'
+                        target='_blank'
+                    >
                         Repo
                     </a>
                 </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,14 @@ const Header = () => {
     return (
         <nav className='light-blue darken-2'>
             <div className='nav-wrapper'>
-                <a href='!#' className='brand-logo'>
+                <a href='/' className='brand-logo'>
                     React Shop
                 </a>
                 <ul id='nav-mobile' className='right hide-on-med-and-down'>
                     <li>
-                        <a href='!#'>Repo</a>
+                        <a href='https://github.com/entryxbad/react-shop' target='_blank' rel='noreferrer'>
+                            Repo
+                        </a>
                     </li>
                 </ul>
             </div>
